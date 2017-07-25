dad523e55d11d4be441b7786bef8cd1ff2749e36
MichaelTrann <mitr5942@gmail.com>
2017-07-24 17:09:30 -0700

    Fixed table tag appearing before wikitable tag

--- parser.js
+++ parser.js
@@ -1,11 +1,12 @@
 console.log("begin");
-var testArr = "1234456789<table class=\"wikitable sortable\">AAAAAAAAAAAAAAAAAAAAA</table>452543543543543";
+var testArr = "32525235</table>1234456789<table class=\"wikitable sortable\">AAAAAAAAAAAAAAAAAAAAA</table>452543543543543";
 
 function parse(array){
 	var beginIndex = array.search("<table class=\"wikitable sortable\">");
-  var endIndex = array.search("</table>");
+  var beginArray = array.slice(beginIndex);
+  var endIndex =  beginArray.search("</table>");
   endIndex = endIndex + 8;
-  var newArray = array.slice(beginIndex,endIndex);
+  var newArray = beginArray.slice(0, endIndex);
   console.log(newArray);
   }
   
